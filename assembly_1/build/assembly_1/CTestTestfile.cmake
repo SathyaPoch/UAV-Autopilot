1f3e14d711660feca098aaf002a02f9cd403264e
Sathya Poch <sathyapochvitamin@gmail.com>
2026-08-28 04:33:41 +0000

    reconnect to px4 with rice_field world

---
+++ assembly_1/build/assembly_1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pochsathya/workingdir/UAV-Autopilot/assembly_1
+# Build directory: /home/pochsathya/workingdir/UAV-Autopilot/assembly_1/build/assembly_1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
